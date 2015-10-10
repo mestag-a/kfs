2100bd6b651830c7a5728ed986c96d7db5dd574e
Alexis Mestag <alexis.mestag@epitech.eu>
2015-10-10 18:44:17 +0200

    printk writes on serial too

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login   <mestag_a@epitech.net>
 ** 
 ** Started on  Fri Oct  9 13:40:50 2015 alexis mestag
-** Last update Sat Oct 10 17:05:58 2015 alexis mestag
+** Last update Sat Oct 10 18:43:32 2015 alexis mestag
 */
 
 typedef	unsigned char	uint8_t;
@@ -13,7 +13,7 @@ typedef	unsigned short	uint16_t;
 
 /*
 ** UART Line control structure
-** For assignment : 8N1: 0b00000011
+** For assignment : 8N1: 0b10000011
 */
 struct		lc_t {
   uint8_t	word_length	: 2; /* 0b11 for 8 bits word length */
@@ -65,7 +65,7 @@ uint16_t	get_cursor() {
   return (c.offset);
 }
 
-void	update_cursor(uint16_t row, uint16_t col) {
+void	set_cursor(uint16_t row, uint16_t col) {
   union cursor	c = { .offset = row * 80 + col };
 
   outb(0x0f, 0x3d4);
@@ -74,50 +74,72 @@ void	update_cursor(uint16_t row, uint16_t col) {
   outb(c.bytes.high, 0x3d5);
 }
 
+void	putc_vga(char c, uint16_t offset, uint16_t *row_ptr, uint16_t *col_ptr) {
+  static struct vga_text_char	*vga = (void *) 0xb8000;
+  uint16_t			row = *row_ptr, col = *col_ptr;
+
+  switch (c) {
+  case '\b':
+    if (col == 0) {
+      col = 79; row--;
+    } else {
+      col--;
+    }
+    break;
+  case '\t':
+    while (++col % 8 != 0);
+    break;
+  case '\n': row++; col = 0;
+    break;
+  case '\v':
+    break;
+  case '\f':
+    break;
+  case '\r': col = 0;
+    break;
+  default:
+    vga[offset].ascii_code = c;
+    if (col == 79) {
+      col = 0; row++;
+    } else {
+      col++;
+    }
+    break;
+  }
+  *row_ptr = row;
+  *col_ptr = col;
+}
+
+void	putc_serial(char c) {
+  outb(c, 0x3f8);
+}
+
 void	printk(const char *msg) {
-  struct vga_text_char	*vga = (void *) 0xb8000;
   uint16_t		offset = get_cursor();
   uint16_t		row = offset / 80, col = offset % 80;
 
   for(int i = 0; msg[i]; ++i) {
-    switch (msg[i]) {
-    case '\b':
-      if (col == 0) {
-	col = 79; row--;
-      } else {
-	col--;
-      }
-      break;
-    case '\t':
-      while (++col % 8 != 0);
-      break;
-    case '\n': row++; col = 0;
-      break;
-    case '\v':
-      break;
-    case '\f':
-      break;
-    case '\r': col = 0;
-      break;
-    default:
-      vga[offset].ascii_code = msg[i];
-      if (col == 79) {
-	col = 0; row++;
-      } else {
-	col++;
-      }
-      break;
-    }    
+    putc_vga(msg[i], offset, &row, &col);
+    putc_serial(msg[i]);
     offset = row * 80 + col;
   }
-  update_cursor(row, col);
+  set_cursor(row, col);
+}
+
+void	init_serial() {
+  /* DLAB bit to allow access to divisor registers */
+  outb(0x08, 0x3f8 + 3); /* LCR */
+
+  /* 38400 bds */
+  outb(0x03, 0x3f8); /* Low byte divisor */
+  outb(0x00, 0x3f8 + 1); /* High byte divisor */
+
+  /* Clear the dlab bit + 8N1 */
+  outb(0x03, 0x3f8 + 3); /* LCR */
 }
 
 int		main() {
+  init_serial();
   printk("Hello World!\n");
-  printk("\bYolo\tooo\rYala\n");
-  printk("0123456789\n");
-  printk("01234567\t89\n");
-  printk("0123456\t789\n");
   return (0);
 }
